b73dfd749383d91ed36cd39ab63708d7f66c9833
ArnabNayan <arnabnayan5@gmail.com>
2023-06-09 14:45:02 +0600

    instructor page data done

--- src/Pages/Instructor/Instructors.jsx
+++ src/Pages/Instructor/Instructors.jsx
@@ -1,15 +1,33 @@
+import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
 
 
 
 
 const Instructors = () => {
+  const [instructors, setInstructors] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:5000/instructors')
+        .then(res => res.json())
+        .then(data => setInstructors(data))
+}, [])
   return (
     <div>
        <Helmet>
             <title>Sports Academy | Instructors</title>
            </Helmet>
-      <h2>This is instructor</h2>
+           <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
+                {instructors.map((instructor, index) => (
+                  <div key={index} className="card card-compact mt-24 w-96 bg-base-100 shadow-xl ">
+                    <figure><img className="w-full h-96" src={instructor.image_url} alt="Instructor" /></figure>
+                    <div className="card-body mx-auto">
+                      <h2 className="card-title font-serif">{instructor.name}</h2>
+                      <p className="card-title font-serif">{instructor.email}</p>
+                    </div>
+                  </div>
+                ))
+            }
+            </div>
     </div>
   );
 };
